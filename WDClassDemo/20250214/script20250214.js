5890a74218dd93bc52de6821c3d436b9ec971c55
COCO-WaterBird <zhangkk533@gmail.com>
2025-03-01 09:04:46 -0800

    learn

--- WDClassDemo/20250214/script20250214.js
+++ WDClassDemo/20250214/script20250214.js
@@ -24,7 +24,7 @@ const todos = [
 
 const todoList = document.getElementById("todos");
 
-// 通过 for 循环从数组最后一个元素开始遍历
+
 for (let i = todos.length - 1; i >= 0; i--) {
     const li = document.createElement("li");
     li.textContent = todos[i];
